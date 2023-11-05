788f33ca13b6cd11c61df1f5d498935e088717b1
Alexey <148199192+A1ikabar@users.noreply.github.com>
2023-11-05 11:36:50 +0300

    Add files via upload

---
+++ Lab#3/Header.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <iostream>
+#include <cctype>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+//void displayBySearch(string name);
+void enter(), init_list(), display();
+void /*update(),*/ input(int i);
+int menu();
+const int SIZE = 100;
---
+++ Lab#3/Lab3 OP.cpp
@@ -0,0 +1,62 @@
+﻿#include "Header.h"
+
+
+/*void displayBySearch(const string& query)
+{
+	cout << "function's work began";
+	string name;
+	ifstream file("Statement.txt");
+	if (file.is_open())
+	{
+		string line;
+		bool found = false;
+		int sessionLines = 0;
+		cout << "Founded result: " << name << endl;
+		while (getline(file, line))
+		{
+			if (sessionLines > 0)
+			{
+				cout << line << endl;
+				sessionLines--;
+				continue;
+			}
+			if (line.find(name) != string::npos)
+			{
+				cout << line << endl;
+				found = true;
+				sessionLines = 2;
+			}
+		}
+		if (!found)
+			cout << "Result not found." << endl;
+		file.close();
+	}
+	else cout << "No saved results" << endl;
+}*/
+
+int main()
+{
+	string name;
+	char choice;
+	init_list();
+	for (;;)
+	{
+		choice = menu();
+		switch (choice)
+		{
+		case 'e': enter(); break;
+		case 'd': display(); break;
+		/*case 'sbn':
+			cout << "Enter the name of magazine: ";
+			cin >> name;
+			displayBySearch(name); 
+			break;*/
+		//case 'u': update(); break;
+		case 'q': return 0;
+		}
+	}
+}
+
+
+
+
