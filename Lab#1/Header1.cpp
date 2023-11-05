cf31d8a27bf68553a7bf267cbf35f4d497424f67
Alexey <148199192+A1ikabar@users.noreply.github.com>
2023-11-05 11:33:55 +0300

    Add files via upload

---
+++ Lab#1/Header1.cpp
@@ -0,0 +1,8 @@
+#include "Header1.h"
+#include <cmath>
+
+double Header1(double a, double b)
+{
+	double result = pow(a, b);
+	return result;
+}
---
+++ Lab#1/Lab1 OP.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+#include "Header1.h"
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    double a, b;
+    int Cn = 1;
+    do {
+        cout << "Введите число: "; cin >> a;
+        cout << "Введите степень: "; cin >> b;
+        cout << Header1(a, b);
+        cout << "\nДля продолжения введите 1.\nДля выхода введите любое другое число." << endl;
+        cin >> Cn;
+    } while (Cn == 1);
+    return 0;
+}
