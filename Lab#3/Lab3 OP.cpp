72e17dbb90eb374338c690c9d3abe15aa2730bf6
Alexey <148199192+A1ikabar@users.noreply.github.com>
2023-11-14 17:07:30 +0300

    Update Lab3 OP.cpp

--- Lab#3/Lab3 OP.cpp
+++ Lab#3/Lab3 OP.cpp
@@ -1,62 +1,58 @@
-﻿#include "Header.h"
+#include <iostream>
+#include <fstream>
+#include "Header.h"
+#include <locale.h>
+#include <windows.h>
 
+using namespace std;
 
-/*void displayBySearch(const string& query)
-{
-	cout << "function's work began";
-	string name;
-	ifstream file("Statement.txt");
-	if (file.is_open())
-	{
-		string line;
-		bool found = false;
-		int sessionLines = 0;
-		cout << "Founded result: " << name << endl;
-		while (getline(file, line))
-		{
-			if (sessionLines > 0)
-			{
-				cout << line << endl;
-				sessionLines--;
-				continue;
-			}
-			if (line.find(name) != string::npos)
-			{
-				cout << line << endl;
-				found = true;
-				sessionLines = 2;
-			}
-		}
-		if (!found)
-			cout << "Result not found." << endl;
-		file.close();
-	}
-	else cout << "No saved results" << endl;
-}*/
-
-int main()
-{
-	string name;
-	char choice;
-	init_list();
-	for (;;)
-	{
-		choice = menu();
-		switch (choice)
-		{
-		case 'e': enter(); break;
-		case 'd': display(); break;
-		/*case 'sbn':
-			cout << "Enter the name of magazine: ";
-			cin >> name;
-			displayBySearch(name); 
-			break;*/
-		//case 'u': update(); break;
-		case 'q': return 0;
-		}
-	}
+void saveToFile(const Magazine& magazine, const string& filename) {
+    ofstream file(filename);
+    if (file.is_open()) {
+        file << "NoM: " << magazine.name << endl;
+        file << "PoM: " << magazine.price << endl;
+        file << "NoP: " << magazine.pages << endl;
+        file << "IS: " << magazine.in_stock << endl;
+        file.close();
+        cout << "Data saved into: " << filename << endl;
+    }
+    else {
+        cout << "Opening ERROR: " << filename << endl;
+    }
 }
 
+Magazine loadFromFile(const string& filename) {
+    ifstream file(filename);
+    Magazine magazine;
+    if (file.is_open()) {
+        file >> magazine.name;
+        file >> magazine.price;
+        file >> magazine.pages;
+        file >> magazine.in_stock;
+        file.close();
+        cout << "Data upload from: " << filename << endl;
+    }
+    else {
+        cout << "Opening ERROR: " << filename << endl;
+    }
+    return magazine;
+}
 
+int main() {
+    setlocale(LC_ALL, "RUS");
+    Magazine magazine;
+    magazine.input();
 
+    cout << "NoF to save: ";
+    string filename;
+    cin >> filename;
+    saveToFile(magazine, filename);
 
+    cout << "NoF for upload: ";
+    cin >> filename;
+    Magazine loadedMagazine = loadFromFile(filename);
+
+    loadedMagazine.display();
+
+    return 0;
+}
