30b4c38296d53a6aab8f99fbe1cb44f62e2f9457
Alexey <148199192+A1ikabar@users.noreply.github.com>
2023-11-17 16:18:56 +0300

    Update Lab3 OP.cpp

--- Lab#3/Lab3 OP.cpp
+++ Lab#3/Lab3 OP.cpp
@@ -1,58 +1,204 @@
 #include <iostream>
-#include <fstream>
+#include <fstream> // Для работы с файлами
 #include "Header.h"
+#include <string>
 #include <locale.h>
 #include <windows.h>
 
 using namespace std;
 
-void saveToFile(const Magazine& magazine, const string& filename) {
-    ofstream file(filename);
-    if (file.is_open()) {
-        file << "NoM: " << magazine.name << endl;
-        file << "PoM: " << magazine.price << endl;
-        file << "NoP: " << magazine.pages << endl;
-        file << "IS: " << magazine.in_stock << endl;
-        file.close();
-        cout << "Data saved into: " << filename << endl;
-    }
-    else {
-        cout << "Opening ERROR: " << filename << endl;
-    }
-}
 
-Magazine loadFromFile(const string& filename) {
-    ifstream file(filename);
-    Magazine magazine;
-    if (file.is_open()) {
-        file >> magazine.name;
-        file >> magazine.price;
-        file >> magazine.pages;
-        file >> magazine.in_stock;
-        file.close();
-        cout << "Data upload from: " << filename << endl;
-    }
-    else {
-        cout << "Opening ERROR: " << filename << endl;
-    }
-    return magazine;
-}
+
 
 int main() {
-    setlocale(LC_ALL, "RUS");
-    Magazine magazine;
-    magazine.input();
+    //Русский язык
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
-    cout << "NoF to save: ";
-    string filename;
-    cin >> filename;
-    saveToFile(magazine, filename);
+    const int MAX_MAGAZINES = 10;
+    Magazine magazines[MAX_MAGAZINES];
+    int numMagazines = 0;
+    int confirm; // переменная для подтверждения очистки файла
+    loadMagazinesFromFile(magazines, numMagazines);
+    int choice;
+    do {
+        cout << "Меню:" << endl;
+        cout << "1. Заполнить информацию о журнале" << endl;
+        cout << "2. Вывести информацию о журналах" << endl;
+        cout << "3. Сохранить информацию в файл" << endl; // МОЖНО УДАЛИТЬ
+        cout << "4. Загрузить информацию из файла" << endl; // МОЖНО УДАЛИТЬ
+        cout << "6. Очистить файл" << endl;
+        cout << "0. Выход" << endl;
+        cout << "Выберите действие: ";
+        cin >> choice;
 
-    cout << "NoF for upload: ";
-    cin >> filename;
-    Magazine loadedMagazine = loadFromFile(filename);
+        switch (choice) {
+        case 1: {
+            if (numMagazines < MAX_MAGAZINES) {
+                Magazine newMagazine;
+                string name;
+                int pages, cost, in_stock;
 
-    loadedMagazine.display();
+                cout << "\nВведите название: ";
+                cin.ignore();
+                getline(cin, name);
+                newMagazine.setName(name);
+
+                cout << "Введите кол-во страниц: ";
+                cin >> pages;
+                newMagazine.setPages(pages);
+
+                cout << "Введите цену: ";
+                cin >> cost;
+                newMagazine.setCost(cost);
+
+                cout << "Введите количество на складе: ";
+                cin >> in_stock;
+                newMagazine.setStock(in_stock);
+
+                if (magazines[numMagazines - 1].number >= 1 && magazines[numMagazines - 1].number <= MAX_MAGAZINES) {
+                    newMagazine.setNumber(magazines[numMagazines - 1].number + 1);
+                }
+                else {
+                    newMagazine.setNumber(1);
+                }
+
+                magazines[numMagazines] = newMagazine;
+                numMagazines++;
+                saveMagazinesToFile(magazines, numMagazines);
+                cout << "Журнал добавлен.\n" << endl;
+            }
+            else {
+                cout << "Достигнуто максимальное количество журналов." << endl;
+            }
+            break;
+        }
+
+        case 2: {
+            for (int i = 0; i < numMagazines; i++) {
+                magazines[i].displayInfo();
+                cout << endl;
+            }
+            break;
+        }
+
+        case 3: {
+            saveMagazinesToFile(magazines, numMagazines);
+            break;
+        }
+
+        case 4: {
+            loadMagazinesFromFile(magazines, numMagazines);
+            break;
+        }
+
+        case 5: {
+            int magazineIndex;
+            cout << "\nВведите номер журнала для обновления информации (0-" << numMagazines - 1 << "): ";
+            cin >> magazineIndex;
+
+            if (magazineIndex >= 0 && magazineIndex < numMagazines) {
+                Magazine& magazine = magazines[magazineIndex];
+
+                int updateChoice;
+                cout << "Меню обновления информации:" << endl;
+                cout << "1. Изменить название" << endl;
+                cout << "2. Изменить кол-во страниц" << endl;
+                cout << "3. Изменить цену" << endl;
+                cout << "4. Изменить кол-во в наличии" << endl;
+                cout << "0. Выход из меню обновления в основное меню" << endl;
+                cout << "Выберите действие: ";
+                cin >> updateChoice;
+
+                switch (updateChoice) {
+                case 1: {
+                    string name;
+                    cout << "\nВведите новое название: ";
+                    cin.ignore();
+                    getline(cin, name);
+                    magazine.setName(name);
+                    break;
+                }
+                case 2: {
+                    int pages;
+                    cout << "Введите новое кол-во страниц: ";
+                    cin.ignore();
+                    cin >> pages;
+                    magazine.setPages(pages);
+                    break;
+                }
+                case 3: {
+                    double cost;
+                    cout << "Введите новую цену: ";
+                    cin.ignore();
+                    cin >> cost;
+                    magazine.setCost(cost);
+                    break;
+                }
+
+                case 4: {
+                    int in_stock;
+                    cout << "\nВведите новое кол-во на складе: ";
+                    cin.ignore();
+                    cin >> in_stock;
+                    magazine.setStock(in_stock);
+                    break;
+                }
+
+                case 0: {
+                    cout << "\nВыйти из меню обновления информации." << endl;
+                    break;
+                }
+
+                default: {
+                    cout << "\nНеверное выбрано действие. Пожалуйста, попробуйте снова." << endl;
+                    break;
+                }
+                }
+
+                saveMagazinesToFile(magazines, numMagazines);
+            }
+
+            else {
+                cout << "Неверный номер журнала." << endl;
+            }
+            break;
+        }
+        case 6: {
+            cout << "Вы точно хотите очистить файл?\nВведите 1, если подтверждаете, либо 0, если хотите отменить очистку: ";
+            cin >> confirm;
+
+            if (confirm == 1) {
+                clearFile(); // вызов функции
+                cout << "Файл очищен" << endl;
+                for (int i = 0; i < MAX_MAGAZINES; i++) {
+                    // Очистка каждого элемента массива структур
+                    magazines[i] = {}; // Присваивание пустого значения по умолчанию
+                }
+
+                Magazine magazines[MAX_MAGAZINES];
+            }
+            else if (confirm == 0) {
+                cout << "Очистка отменена" << endl;
+            }
+            else {
+                cout << "Введите 1 или 0" << endl;
+            }
+            break;
+        }
+        case 0: {
+            cout << "Выход из программы." << endl;
+            break;
+        }
+
+        default: {
+            cout << "Неверное выбрано действие. Пожалуйста, попробуйте снова." << endl;
+            break;
+        }
+        }
+
+        cout << endl;
+    } while (choice != 0);
 
     return 0;
 }
