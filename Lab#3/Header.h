760c2453c69a6f299e355d3479f4e4904ceca35e
Alexey <148199192+A1ikabar@users.noreply.github.com>
2023-11-17 16:18:41 +0300

    Update Header.h

--- Lab#3/Header.h
+++ Lab#3/Header.h
@@ -1,18 +1,38 @@
 #pragma once
-
 #ifndef HEADER_H
 #define HEADER_H
 
 #include <string>
+#include <fstream> // Для работы с файлами
+using namespace std;
 
 struct Magazine {
-    std::string name;
-    double price;
+    int number;
+    string name;
     int pages;
+    int cost;
     int in_stock;
 
-    void input();  // input method
-    void display();  // output method
+    // Метод для присвоения порядкового номера
+    void setNumber(int newNumber);
+
+    // Метод для изменения названия журнала
+    void setName(string newName);
+
+    // Метод для изменения количества страниц
+    void setPages(int newPages);
+
+    // Метод для изменения цены журнала
+    void setCost(double newCost);
+
+    // Метод для изменения kol-va na sklade
+    void setStock(int newIn_stock);
+
+    // Метод для вывода журналов из файла
+    void displayInfo();
 };
 
+void loadMagazinesFromFile(Magazine magazines[], int& numMagazines);
+void saveMagazinesToFile(Magazine magazines[], int numMagazines);
+bool clearFile();
 #endif
